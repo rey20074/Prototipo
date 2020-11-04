a16874eca8b3e62c2c5f051229ed38b76b3be83d
andreadelou <70552370+andreadelou@users.noreply.github.com>
2020-11-04 15:38:21 -0600

    Add files via upload

---
+++ Lectura.java
@@ -0,0 +1,15 @@
+/*
+Página de Lectura
+Andrea Lam, 20102
+El objetivo principal de esta clase es generar un método abstracto para la implementación
+de herencia y polimorfismo para la lectura de los archivos de csv en las diferentes clases
+Ultima Modificación:
+12/11/2020
+
+*/
+
+public abstract class Lectura {
+
+    public abstract void leer();
+
+}
